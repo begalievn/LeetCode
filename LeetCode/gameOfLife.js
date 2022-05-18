d1fcd69ce59fa1faf195d85b568d89e73692e469
begalievn <begaliev.info@gmail.com>
2022-05-19 01:07:31 +0600

    minAbsoluteSumDiff1818.js added

---
+++ LeetCode/gameOfLife.js
@@ -0,0 +1,11 @@
+var gameOfLife = function (board) {
+  return board;
+};
+
+let board = [
+  [0, 1, 0],
+  [0, 0, 1],
+  [1, 1, 1],
+  [0, 0, 0],
+];
+console.log(gameOfLife(board));
---
+++ LeetCode/getMinSwaps1850.js
@@ -0,0 +1,9 @@
+var getMinSwaps = function (num, k) {
+  let digits = [...num];
+  return digits;
+};
+
+let num = "5489355142",
+  k = 4;
+
+console.log(getMinSwaps(num, k));
